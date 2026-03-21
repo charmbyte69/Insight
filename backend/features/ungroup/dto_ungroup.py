719ae1e8eaab8f45f2a1e8d50f5af1b7912976a4
Marvz02 <marvenhangad1232gmail.com>
2026-03-21 10:38:25 +0800

    UNGROUP

--- backend/features/ungroup/dto_ungroup.py
+++ backend/features/ungroup/dto_ungroup.py
@@ -1,12 +1,21 @@
-from pydantic import BaseModel, Field
-from typing import List
+from pydantic import BaseModel
+from typing import List, Dict
 
 class SampleRequestDTO(BaseModel):
     min: int
     max: int
     values: List[int]
 
+class UngroupedDataResponse(BaseModel):
+    mean: float
+    mode: List[float]
+    median: float
+    frequency_table: Dict[int, int]
+    sorted_dataset: List[float]
+
 class SampleResponseDTO(BaseModel):
     Instructor_ID: int
     message: str
     filtered_values: List[int]
+    # This is where the "passed" data results will live
+    statistics: UngroupedDataResponse
